6ed12f59fb6cd94db92fbf4c529ebccde8d70bef
xanderez17 <kevxanderez17@gmail.com>
2022-05-12 11:30:34 -0500

    Encabezado-PiePágina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
+import { CuerpoComponent } from './components/cuerpo/cuerpo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PiePaginaComponent,
+    CuerpoComponent
   ],
   imports: [
     BrowserModule,
