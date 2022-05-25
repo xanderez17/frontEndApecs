df2ff02059abe36d3285c88d7ea5fc53f357844f
icrv93 <icrv93@gmail.com>
2022-05-25 11:03:52 -0500

    sidenav

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -1,28 +1,78 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SidebarService {
-
-  constructor() { }
+  constructor() {}
 
   menu: any[] = [
-    
     {
       titulo: 'Gestión de Cursos ',
-      
       submenu: [
         {
-          titulo: 'Crear Curso',
-         
-          url: 'crear-curso',
+          titulo: 'Crear Catalogo',
+          url: 'crear-catalogo',
+        },
+        {
+          titulo: 'Ver Catalogos',
+          url: 'listar-catalogos',
         },
         {
           titulo: 'Listar curso',
-       
           url: 'listar-cursos',
+        },
+      ],
+    },
+    {
+      titulo: 'Gestión de Contratos ',
+      submenu: [
+        {
+          titulo: 'Crear matriculas',
+          url: 'crear-matricula',
+        },
+        {
+          titulo: 'Ver Matriculas',
+          url: 'listar-matriculas',
+        },
+
+        {
+          titulo: 'Ver Contratos',
+          url: 'listar-contratos',
         }
+
       ]
-    }];
+    },
+    {
+      titulo: 'Gestión de Personas ',
+      submenu: [
+        {
+          titulo: 'Crear Alumno',
+          url: 'inscripcion',
+        },
+        {
+          titulo: 'Ver Alumnos',
+          url: 'listar-alumnos',
+        },
+        {
+          titulo: 'Crear Docentes',
+          url: 'crear-docente',
+        },
+        {
+          titulo: 'Ver Docentes',
+          url: 'listar-docentes',
+        },  {
+          titulo: 'Crear Representante',
+          url: 'editar-representante',
+        },
+        {
+          titulo: 'Ver Represententes',
+          url: 'listar-representantes',
+        },
+      ],
+    },
+
+
+
+  ];
 }
